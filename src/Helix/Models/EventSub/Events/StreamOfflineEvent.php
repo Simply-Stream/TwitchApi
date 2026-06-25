acd5dd9adcfce3b1051f78a28c86aa406d44ea73
tobidev_ <tobidev@simply-stream.com>
2026-06-25 21:11:29 +0900

    fix: Fixed faulty model

--- src/Helix/Models/EventSub/Events/StreamOfflineEvent.php
+++ src/Helix/Models/EventSub/Events/StreamOfflineEvent.php
@@ -7,15 +7,22 @@ namespace SimplyStream\TwitchApi\Helix\Models\EventSub\Events;
 final readonly class StreamOfflineEvent extends Event
 {
     /**
+     * @param string $id The id of the stream - a Parameter that Twitch, yet again, just changed without documentation.
      * @param string $broadcasterUserId    The broadcaster’s user id.
      * @param string $broadcasterUserLogin The broadcaster’s user login.
      * @param string $broadcasterUserName  The broadcaster’s user display name.
      */
     public function __construct(
+        private string $id,
         private string $broadcasterUserId,
         private string $broadcasterUserLogin,
         private string $broadcasterUserName,
     ) {
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
     }
 
     public function getBroadcasterUserId(): string
