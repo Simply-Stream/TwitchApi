7b5548095581130e93cd6ffdc0af3aa589ace1a4
tobidev_ <tobidev@simply-stream.com>
2026-07-15 10:54:32 +0200

    refactor: Deleted old request class and obsolete use statement in CharityCampaign.php

--- src/Helix/Models/Charity/CharityCampaign.php
+++ src/Helix/Models/Charity/CharityCampaign.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace SimplyStream\TwitchApi\Helix\Models\Charity;
 
-use SimplyStream\TwitchApi\EventSub\Shared\Amount;
-
 final readonly class CharityCampaign
 {
     /**
