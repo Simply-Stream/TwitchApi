e8961c84b30d38a807d9b8779984b9cd530845dd
tobidev_ <tobidev@simply-stream.com>
2026-09-12 23:54:44 +0200

    fix: Fixed a typo

--- src/Helix/Api/Users/AuthorizationUser.php
+++ src/Helix/Api/Users/AuthorizationUser.php
@@ -10,14 +10,14 @@ final readonly class AuthorizationUser {
      * @param string         $userName The user’s display name.
      * @param string         $userLogin The user’s login name.
      * @param array<string>  $scopes An array of all the scopes the user has granted to the client ID.
-     * @param bool           $hasAuthorization A boolean indicating whether or not the specified user has authorized this application.
+     * @param bool           $hasAuthorized A boolean indicating whether or not the specified user has authorized this application.
      */
     public function __construct(
         public string $userId,
         public string $userName,
         public string $userLogin,
         public array $scopes,
-        public bool $hasAuthorization,
+        public bool $hasAuthorized,
     ) {
     }
 }
